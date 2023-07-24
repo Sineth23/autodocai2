6b82452d35b763755160f0ce9da0da28c0d038d2
Sineth23 <57430821+Sineth23@users.noreply.github.com>
2023-07-24 13:16:49 -0400

    Update WaitUtil.py

--- cli/utils/WaitUtil.py
+++ cli/utils/WaitUtil.py
@@ -1,1 +1,40 @@
 
+import asyncio
+
+async def wait(timeout_ms, value=None):
+    await asyncio.sleep(timeout_ms / 1000)
+    return value
+
+async def for_true(fn):
+    count = 0
+    while not fn():
+        await asyncio.sleep(0.05)
+        count += 1
+        if count >= 200:
+            raise TimeoutError()
+    return True
+
+# main.py
+
+#import asyncio
+#from helpers import wait, for_true
+
+#async def main():
+   #try:
+  #      # Usage of wait
+   #     result = await wait(1000, 'Hello')
+    #    print(result)  # Output: 'Hello'
+#
+ #       # Usage of for_true
+  #      def check_condition():
+            # Replace this function with your own boolean condition
+            # Example: return some_variable == expected_value
+   #         return False
+
+    #    await for_true(check_condition)
+     #   print('Condition became True!')
+   # except TimeoutError:
+    #    print('Timeout: Condition did not become True within 10 seconds.')
+
+#if __name__ == "__main__":
+#    asyncio.run(main())
