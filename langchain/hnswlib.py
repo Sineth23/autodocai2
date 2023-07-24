a7d6a6e23128fa323c61293c7e7f69c4038dbf1f
Sineth23 <57430821+Sineth23@users.noreply.github.com>
2023-07-24 14:55:55 -0400

    Update hnswlib.py

--- langchain/hnswlib.py
+++ langchain/hnswlib.py
@@ -1,1 +1,54 @@
-lang
+import os
+import json
+import hnswlib
+import numpy as np
+
+class Document:
+    def __init__(self, pageContent, metadata):
+        self.pageContent = pageContent
+        self.metadata = metadata
+
+class InMemoryDocstore:
+    def __init__(self):
+        self._docs = {}
+
+    def add(self, data):
+        self._docs.update(data)
+
+    def search(self, docIndex):
+        return self._docs.get(docIndex, None)
+
+class Embeddings:
+    @staticmethod
+    def embedDocuments(texts):
+        # Replace this with actual document embeddings logic
+        num_dimensions = 10
+        return np.random.rand(len(texts), num_dimensions)
+
+class SaveableVectorStore:
+    def __init__(self, embeddings, args):
+        self.embeddings = embeddings
+        self.args = args
+
+    # Your SaveableVectorStore implementation here
+
+class HNSWLibBase:
+    def __init__(self, space, numDimensions=None):
+        self.space = space
+        self.numDimensions = numDimensions
+
+class HNSWLibArgs(HNSWLibBase):
+    def __init__(self, space, numDimensions=None, docstore=None, index=None):
+        super().__init__(space, numDimensions)
+        self.docstore = docstore
+        self.index = index
+
+class HNSWLib(SaveableVectorStore):
+    def __init__(self, embeddings, args):
+        super().__init__(embeddings, args)
+        self._index = args.index
+        self.docstore = args.docstore if args.docstore else InMemoryDocstore()
+
+    # Your HNSWLib implementation here
+
+
