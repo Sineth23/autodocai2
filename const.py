293122c22ed0d7820eee2f96747b3fdf6d459fb1
Sineth23 <57430821+Sineth23@users.noreply.github.com>
2023-07-24 09:35:56 -0400

    Update const.py

--- const.py
+++ const.py
@@ -1,1 +1,6 @@
 
+import os
+from pathlib import Path
+
+user_config_file_name = 'autodoc.user.json'
+user_config_file_path = Path(os.path.expanduser('~'), '.config', 'autodoc', user_config_file_name)
